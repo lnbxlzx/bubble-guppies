1c5af4a456269a45ad9300955934d2306c66e333
lnbxlzx <156631101+lnbxlzx@users.noreply.github.com>
2024-01-18 01:38:17 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -17,3 +17,21 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #get the Day, Items, Note, Amount for each record
         #and append to the overheads list
         overheads.append([row[0],row[1],row[2],row[3]]) 
+        
+# def percentage_calculator(Items,total_overheads):
+#     """
+#     function to calculate the percentage of each item in the overheads
+#     """
+#     return (total_amount / total_overheads) * 100
+
+
+total_overheads = 0
+
+for row in overheads:
+
+    # read the row
+    Day = row[0]
+    Items = row[1] 
+    Amount = float(row[3].replace("$",""))
+
+    total_overheads += Amount
