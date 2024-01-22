0c604021d2c41b611163f09d974ebce7c4a1628c
destonwong <156265559+destonwong@users.noreply.github.com>
2024-01-22 11:53:30 +0800

    does not wokr

--- cashonhand.py
+++ cashonhand.py
@@ -1,9 +1,6 @@
 import csv
 from pathlib import Path
 
-deficit = []
-surplus = []
-
 # create a file path to csv file.
 fp = Path.cwd()/"cashonhand.csv"
 
@@ -14,15 +11,21 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 
     # create an empty list for delivery record
     cash_on_hand=[] 
-
+    deficit = []
+    surplus = []
+    
     # append cash on hand into the cash_on_hand list
     for row in reader:
         #get the day and amount for each record
         #and append to the cash on hand list
         cash_on_hand.append([row[0],row[1]]) 
 
-
+highest_surplus = 0
+highest_deficit = 0
+day = None
+final_surplus = 0
 cash_on_hand_diff = 0
+
 for row in cash_on_hand:
     day_number = row[0]
     s_amount = row[1]
@@ -35,8 +38,7 @@ for row in cash_on_hand:
                 cashsurplus = s_amount - prev_amount
             else:
                 None
-        return 
-            
+                
     if cashsurplus > highest_surplus:
         highest_surplus = final_surplus
         day_number = day
@@ -48,7 +50,7 @@ output_file = "Summary_report.txt"
 with open(output_file, "w") as file:
 
     #write driver data in txt file
-
-    file.write(f"[HIGHEST OVERHEADS] {highest_percentage_overheads} {highest_percentage}%")
+    file.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    file.write(f"[HIGHEST CASH SURPLUS] {day} {highest_surplus}%")
 
     
